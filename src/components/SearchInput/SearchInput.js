4a4df5cd3771c2edd055ef336d77ed16c1f2c071
tommy-carlos williams <tommy@devgeeks.org>
2016-09-01 09:56:35 +1000

    Changed TextInput and SearchInput to be stateful
    This is to allow them to have refs (used for getting their input values)
    
    This is a temp solution until they are managed components

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import classNames from 'classnames';
 
-// @TODO - search inputs should be managed
-//   ...or at least be able to be managed
-// @TODO - search icon is missing... need to figure
-//   out webpack issues around this
-const SearchInput = (props) => {
-  const { large = false, ...rest } = props;
-  const cx = classNames({
-    'topcoat-search-input': !large,
-    'topcoat-search-input--large': large,
-  });
-  return (
-    <input type="search" placeholder="search" className={ cx } { ...rest } />
-  );
-};
+export default React.createClass({
 
-SearchInput.displayName = 'SearchInput';
+  displayName: 'SearchInput',
 
-SearchInput.propTypes = {
-  large: React.PropTypes.bool,
-};
+  propTypes: {
+    large: React.PropTypes.bool,
+  },
 
-export default SearchInput;
+  render() {
+    // @TODO - search inputs should be managed
+    //   ...or at least be able to be managed
+    // @TODO - search icon is missing... need to figure
+    //   out webpack issues around this
+    const { large = false, ...rest } = this.props;
+    const cx = classNames({
+      'topcoat-search-input': !large,
+      'topcoat-search-input--large': large,
+    });
+    return (
+      <input
+        ref="search"
+        type="search"
+        placeholder="search"
+        className={ cx } { ...rest }
+      />
+    );
+  },
+});
--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import classNames from 'classnames';
 
-// @TODO - text inputs should be managed
-//   ...or at least be able to be managed
-const TextInput = (props) => {
-  const { full = false, large = false, ...rest } = props;
-  const cx = classNames({
-    full,
-    'topcoat-text-input': !large,
-    'topcoat-text-input--large': large,
-  });
-  return (
-    <input type="text" className={ cx } { ...rest } />
-  );
-};
+export default React.createClass({
 
-TextInput.displayName = 'TextInput';
+  displayName: 'TextInput',
 
-TextInput.propTypes = {
-  full: React.PropTypes.bool,
-  large: React.PropTypes.bool,
-};
+  propTypes: {
+    full: React.PropTypes.bool,
+    large: React.PropTypes.bool,
+  },
 
-export default TextInput;
+  render() {
+    const { full = false, large = false, ...rest } = this.props;
+    const cx = classNames({
+      full,
+      'topcoat-text-input': !large,
+      'topcoat-text-input--large': large,
+    });
+    return (
+      <input
+        ref="input"
+        type="text"
+        className={ cx }
+        { ...rest }
+      />
+    );
+  },
+});
