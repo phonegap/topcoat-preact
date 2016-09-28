9e3e12dbdbdac9dd92d24ab7a024a7268805ca42
tommy-carlos williams <tommy@devgeeks.org>
2016-09-28 11:27:48 +1000

    No need for TODO

--- src/components/Range/stories/index.js
+++ src/components/Range/stories/index.js
@@ -3,7 +3,6 @@ import { storiesOf } from '@kadira/storybook';
 
 import Range from '../';
 
-// @TODO why is the margin-top needed?
 storiesOf('Range', module)
   .addWithInfo('default', () => (
     <Range name="default" />
