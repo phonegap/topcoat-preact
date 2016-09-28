485b2288aec12f86f75f7600bdb5cad12adb2bdf
tommy-carlos williams <tommy@devgeeks.org>
2016-09-28 11:19:56 +1000

    Fixed Range

--- src/components/Range/stories/index.js
+++ src/components/Range/stories/index.js
@@ -6,8 +6,8 @@ import Range from '../';
 // @TODO why is the margin-top needed?
 storiesOf('Range', module)
   .addWithInfo('default', () => (
-    <Range name="default" style={ { marginTop: '20px' } } />
+    <Range name="default" />
   ))
   .addWithInfo('disabled', () => (
-    <Range name="disabled" style={ { marginTop: '20px' } } disabled />
+    <Range name="disabled" disabled />
   ));
